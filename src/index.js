010022d899e58aa1ab0f521cc4a1dc79a92cc599
luca.favorido <luca.favorido@beantech.it>
2024-05-07 16:11:15 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import { initializeIcons } from '@fluentui/font-icons-mdl2';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
 initializeIcons();
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
